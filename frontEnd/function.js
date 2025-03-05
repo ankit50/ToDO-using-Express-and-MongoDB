3eb17cd9de4f133d5c01f7a90efad2436d5ae835
Ankit Kharel <ankeetkharel@gmail.com>
2025-03-05 14:16:34 +0545

    completed edit function

--- frontEnd/function.js
+++ frontEnd/function.js
@@ -28,15 +28,86 @@ export function addSingleTaskToHTMLList(task) {
     deleteIcon.onclick = () => {
         deleteTaskFromHTMLList(task._id);
     }
-    // editIcon.onclick = () => {
-    //     editTaskFromHTMLList(task._id);
-    // }
+    checkBox.onclick = () => {
+        changeStatusFromHTMLList(task._id);
+    }
+    editIcon.onclick = () => {
+        editTaskFromHTMLList(task._id);
+    }
 }
 async function deleteTaskFromHTMLList(id) {
-        const taskId = document.querySelector(`[data-id="${id}"]`);
+    const taskId = document.querySelector(`[data-id="${id}"]`);
+    const response = await fetch(`/api/v1/tasks/${id}`, {
+        method: 'DELETE',
+    });
+    taskId.remove();
+}
+async function changeStatusFromHTMLList(id) {
+    const taskId = document.querySelector(`[data-id="${id}"]`);
+    const checkBox = taskId.querySelector("input[type='checkbox'");
+    const para = taskId.querySelector("p");
+    try {
         const response = await fetch(`/api/v1/tasks/${id}`, {
-            method:'DELETE',
+            method: 'PATCH',
+            headers: { 'Content-Type': 'application/json', },
+            body: JSON.stringify({ completed: checkBox.checked }),
         });
-        taskId.remove();
+        if (checkBox.checked) {
+            para.style.textDecoration = 'line-through';
+            para.style.textDecorationColor = 'red';
+            para.style.textDecorationThickness = '3px';
+        } else {
+            para.style.textDecoration = 'none';
+        }
+    } catch (error) {
+        alert(error);
+    }
+}
 
+async function editTaskFromHTMLList(id) {
+    const taskId = document.querySelector(`[data-id="${id}"]`);
+    const para = taskId.querySelector("p");
+    const checkBox = taskId.querySelector("input[type='checkbox'");
+    const dialog = document.createElement('dialog');
+    dialog.setAttribute('id', 'dialog');
+    dialog.innerHTML = `
+        <p>Edit Task</p>
+        <input type="text" id="dynamicInput" value="${para.textContent}">
+        <input type="checkbox" id="check">
+        <button id="saveBtn">Save</button>
+        <button id="closeBtn">Cancel</button>
+    `;
+    document.body.appendChild(dialog);
+    dialog.showModal();
+    const save = document.getElementById('saveBtn');
+    const check = document.getElementById('check');
+    document.getElementById('closeBtn').addEventListener('click', () => {
+        dialog.remove();
+    });
+    save.addEventListener('click', async (e)=>{
+        const newTask = document.getElementById('dynamicInput').value.trim();
+        try {
+            const response = await fetch(`/api/v1/tasks/${id}`, {
+             method: 'PATCH',
+             headers: { 'Content-Type': 'application/json', },
+             body: JSON.stringify({ name:newTask, completed: check.checked }),
+            });
+            const data = await response.json();
+            console.log(data);
+
+        } catch (error) {
+            alert(error);
+        }
+        if(check.checked){
+            checkBox.checked=true;
+            para.style.textDecoration = 'line-through';
+            para.style.textDecorationColor = 'red';
+            para.style.textDecorationThickness = '3px';
+        } else {
+            checkBox.checked=false;
+            para.style.textDecoration = 'none';
+        }
+        para.textContent=newTask;
+        dialog.remove();
+    });
 }
